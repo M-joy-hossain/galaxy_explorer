435b64f3d8fcc9d023908a0d78e53942805fa900
asmjehan <53009596+asmjehan@users.noreply.github.com>
2026-04-19 23:30:23 +0600

    fix(android setup): Fix gradle file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
-    id 'com.google.gms.google-services'
-
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -22,22 +20,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.galaxy_explorer"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-       // minSdkVersion 21
-
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -46,7 +37,8 @@ android {
 flutter {
     source = "../.."
 }
+
 dependencies {
-    implementation platform('com.google.firebase:firebase-bom:34.12.0')
-    implementation 'com.google.firebase:firebase-analytics'
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
